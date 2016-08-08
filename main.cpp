16356424cd4b9f82f55eb1f405499a2af886c3bc
Michael Schellenberger Costa <mschellenbergercosta@gmail.com>
2016-08-08 14:19:32 +0200

    Added the possibility to pass an ode file as an argument

--- main.cpp
+++ main.cpp
@@ -2,10 +2,14 @@
 #include <string>
 #include "xppParser.h"
 
-int main()
+int main(int argc, char** argv)
 {
-
-	std::string filename = "../Test.ode";
+	std::string filename;
+	if (argc < 2) {
+		filename = "../Test.ode";
+	} else {
+		filename = std::string(argv[1]);
+	}
 	xppParser ParseOde(filename);
 
 	return 0;
